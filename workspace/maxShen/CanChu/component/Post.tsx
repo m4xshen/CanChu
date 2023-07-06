e7336d0d5daeeb5ccdba42d1337631c494fc4994
Max Shen <m4xshen@gmail.com>
2023-07-06 11:30:33 +0800

    Migrate SCSS to styled-components

--- workspace/maxShen/CanChu/component/Post.tsx
+++ workspace/maxShen/CanChu/component/Post.tsx
@@ -1,39 +1,144 @@
-import styles from './styles.module.scss';
+import styled from 'styled-components';
+
+const Feed = styled.div`
+  width: 700px;
+  border: 1px #d3d3d3;
+  border-radius: 20px;
+  background-color: #ffffff;
+`;
+
+const Content = styled.div`
+  padding: 20px 40px 0 40px;
+`;
+
+const FeedTop = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+const Avatar = styled.div`
+  width: 75px;
+  height: 75px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const GrayAvatar = styled(Avatar)`
+  background-color: #d3d3d3;
+`;
+
+const SmallAvatar = styled(Avatar)`
+  width: 30px;
+  height: 30px;
+`;
+
+const SmallRedAvatar = styled(SmallAvatar)`
+  background-color: #f85c53;
+`;
+
+const SmallGrayAvatar = styled(SmallAvatar)`
+  background-color: #d3d3d3;
+`;
+
+const CommentAvatar = styled.img`
+  margin-left: 40px;
+`;
+
+const Block = styled.div`
+  display: block;
+`;
+
+const Name = styled.div`
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+`;
+
+const Time = styled.div`
+  font-size: 13px;
+  font-weight: 400;
+  color: #909090;
+`;
+
+const Article = styled.article`
+  height: 59px;
+  margin: 17px 0 41px 0;
+`;
+
+const Viewers = styled.div`
+  display: flex;
+  padding: 10px 0;
+  gap: 10px;
+  border-top: 1px solid #bfbfbf;
+  border-bottom: 1px solid #bfbfbf;
+`;
+
+const ViewersInfo = styled.div`
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  font-size: 16px;
+  font-weight: 400;
+  color: #5c5c5c;
+`;
+
+const Comment = styled.div`
+  border-top: 1px solid #d9d9d9;
+  display: flex;
+  gap: 15px;
+  align-items: center;
+  height: 90px;
+`;
+
+const CommentButton = styled.div`
+  width: 555px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  border-radius: 30px;
+  border: none;
+  font-size: 20px;
+  font-weight: 400;
+  color: #777777;
+  background-color: #f0f0f0;
+`;
 
 const Post = () => {
   return (
-    <div className={styles.feed}>
-      <div className={styles.content}>
-        <div className={styles["feed-top"]}>
-          <div className={`${styles.avatar} ${styles.gray}`}></div>
-          <div className={styles.info}>
-            <div className={styles.name}>你的朋友</div>
-            <div className={styles.time}>一小時前</div>
-          </div>
-        </div>
-        <article className={styles.text}>
+    <Feed>
+      <Content>
+        <FeedTop>
+          <GrayAvatar />
+          <Block>
+            <Name>你的朋友</Name>
+            <Time>一小時前</Time>
+          </Block>
+        </FeedTop>
+        <Article>
           動態動態動態動態動態動態，動態動態動態動態。
-        </article>
-        <div className={styles.viewers}>
-          <div className={`${styles.avatar} ${styles.viewer} ${styles.red}`}></div>
-          <div className={`${styles.avatar} ${styles.viewer} ${styles.gray}`}></div>
-        </div>
-        <div className={styles["viewers-info"]}>
-          <div className={styles["likes-count"]}>
+        </Article>
+        <Viewers>
+          <SmallRedAvatar></SmallRedAvatar>
+          <SmallGrayAvatar></SmallGrayAvatar>
+        </Viewers>
+        <ViewersInfo>
+          <div>
             7 人喜歡這則貼文
           </div>
-          <div className={styles["comments-count"]}>
+          <div>
             1 則留言
           </div>
-        </div>
-      </div>
-      <div className={styles.comment}>
-        <img className={styles["comment-avatar"]} src="avatar.png" />
-        <button className={styles["comment-button"]}>
+        </ViewersInfo>
+      </Content>
+      <Comment>
+        <CommentAvatar src="avatar.png" />
+        <CommentButton>
           留個言吧
-        </button>
-      </div>
-    </div>
+        </CommentButton>
+      </Comment>
+    </Feed>
   );
 };
 
