2e2d4cf0504171288bc435d137307246962ac1e5
Max Shen <m4xshen@gmail.com>
2023-07-06 12:08:54 +0800

    Migrate styled-components to Tailwind

--- workspace/maxShen/CanChu/components/Post.tsx
+++ workspace/maxShen/CanChu/components/Post.tsx
@@ -1,144 +1,48 @@
-import styled from 'styled-components';
-
-const Feed = styled.div`
-  width: 700px;
-  border: 1px #d3d3d3;
-  border-radius: 20px;
-  background-color: #ffffff;
-`;
-
-const Content = styled.div`
-  padding: 20px 40px 0 40px;
-`;
-
-const FeedTop = styled.div`
-  width: 100%;
-  display: flex;
-  gap: 10px;
-  align-items: center;
-`;
-
-const Avatar = styled.div`
-  width: 75px;
-  height: 75px;
-  border-radius: 50%;
-  cursor: pointer;
-`;
-
-const GrayAvatar = styled(Avatar)`
-  background-color: #d3d3d3;
-`;
-
-const SmallAvatar = styled(Avatar)`
-  width: 30px;
-  height: 30px;
-`;
-
-const SmallRedAvatar = styled(SmallAvatar)`
-  background-color: #f85c53;
-`;
-
-const SmallGrayAvatar = styled(SmallAvatar)`
-  background-color: #d3d3d3;
-`;
-
-const CommentAvatar = styled.img`
-  margin-left: 40px;
-`;
-
-const Block = styled.div`
-  display: block;
-`;
-
-const Name = styled.div`
-  font-size: 16px;
-  font-weight: 700;
-  cursor: pointer;
-`;
-
-const Time = styled.div`
-  font-size: 13px;
-  font-weight: 400;
-  color: #909090;
-`;
-
-const Article = styled.article`
-  height: 59px;
-  margin: 17px 0 41px 0;
-`;
-
-const Viewers = styled.div`
-  display: flex;
-  padding: 10px 0;
-  gap: 10px;
-  border-top: 1px solid #bfbfbf;
-  border-bottom: 1px solid #bfbfbf;
-`;
-
-const ViewersInfo = styled.div`
-  padding: 10px 0;
-  display: flex;
-  justify-content: space-between;
-  font-size: 16px;
-  font-weight: 400;
-  color: #5c5c5c;
-`;
-
-const Comment = styled.div`
-  border-top: 1px solid #d9d9d9;
-  display: flex;
-  gap: 15px;
-  align-items: center;
-  height: 90px;
-`;
-
-const CommentButton = styled.div`
-  width: 555px;
-  height: 50px;
-  line-height: 50px;
-  text-align: center;
-  border-radius: 30px;
-  border: none;
-  font-size: 20px;
-  font-weight: 400;
-  color: #777777;
-  background-color: #f0f0f0;
-`;
+import Image from 'next/image';
 
 const Post = () => {
   return (
-    <Feed>
-      <Content>
-        <FeedTop>
-          <GrayAvatar />
-          <Block>
-            <Name>你的朋友</Name>
-            <Time>一小時前</Time>
-          </Block>
-        </FeedTop>
-        <Article>
+    <div className="w-[700px] border-[#d3d3d3] rounded-[20px] bg-white">
+      <div className="pt-[20px] px-[40px]">
+        <div className="w-full flex gap-[10px] items-center">
+          <div className="w-[75px] h-[75px] rounded-full cursor-pointer bg-[#d3d3d3]" />
+          <div>
+            <div className="text-[16px] font-bold cursor-pointer">你的朋友</div>
+            <div className="text-[13px] font-normal text-[#909090]">一小時前</div>
+          </div>
+        </div>
+        <article className="h-[59px] mt-[17px] mb-[41px]">
           動態動態動態動態動態動態，動態動態動態動態。
-        </Article>
-        <Viewers>
-          <SmallRedAvatar></SmallRedAvatar>
-          <SmallGrayAvatar></SmallGrayAvatar>
-        </Viewers>
-        <ViewersInfo>
+        </article>
+        <div className="flex py-[10px] gap-[10px] border-t border-[#bfbfbf] border-b">
+          <div className="w-[30px] h-[30px] rounded-full cursor-pointer bg-[#f85c53]" />
+          <div className="w-[30px] h-[30px] rounded-full cursor-pointer bg-[#d3d3d3]" />
+        </div>
+        <div className="py-[10px] flex justify-between text-[16px] font-normal text-[#5c5c5c]">
           <div>
             7 人喜歡這則貼文
           </div>
           <div>
             1 則留言
           </div>
-        </ViewersInfo>
-      </Content>
-      <Comment>
-        <CommentAvatar src="avatar.png" />
-        <CommentButton>
+        </div>
+      </div>
+      <div className="border-t border-t-[#d9d9d9] flex gap-[15px] items-center h-[90px]">
+        <Image
+          src="/avatar.png"
+          width={50}
+          height={50}
+          alt="user avatar"
+          className="ml-[40px]"
+        />
+        <div
+          className="w-[555px] h-[50px] leading-[50px] text-center rounded-[30px] border-none
+          text-[20px] font-normal text-[#777777] bg-[#f0f0f0]"
+        >
           留個言吧
-        </CommentButton>
-      </Comment>
-    </Feed>
+        </div>
+      </div>
+    </div>
   );
 };
 
