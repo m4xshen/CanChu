f2261b7d81fad577a13fecb17f937de1fb155ccc
Max Shen <m4xshen@gmail.com>
2023-07-07 15:07:57 +0800

    Add dropdown menu when hovering avatar

--- workspace/maxShen/CanChu/components/navbar/index.tsx
+++ workspace/maxShen/CanChu/components/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Pattaya } from 'next/font/google';
+import { useState } from 'react';
 
 const pattaya = Pattaya({
   weight: '400',
@@ -8,6 +9,8 @@ const pattaya = Pattaya({
 });
 
 const Navbar = () => {
+  const [display, setDisplay] = useState(false);
+
   return (
     <div className="h-[100px] bg-white flex items-center border-b border-[#d9d9d9]">
       <Link href="/">
@@ -31,14 +34,60 @@ const Navbar = () => {
           className="ml-[7px] w-full bg-[#f0f2f5] text-[#566470] outline-0"
         />
       </div>
-      <Link href="/" className="ml-auto mr-[141px]">
-        <Image
-          src="/avatar.png"
-          width={36}
-          height={36}
-          alt="user avatar"
-        />
-      </Link>
+      <div
+        className="ml-auto mr-[141px] relative"
+        onMouseLeave={() => setDisplay(false)}
+      >
+        <Link
+          href="/"
+          onMouseEnter={() => setDisplay(true)}
+        >
+          <Image
+            src="/avatar.png"
+            width={36}
+            height={36}
+            alt="user avatar"
+          />
+        </Link>
+        {
+          display && 
+            <div className="absolute right-0 top-[36px]">
+              <div className="h-[39px] bg-transparent"></div>
+              <nav
+                className="flex flex-col border border-[#0000001A]
+                w-[261px] rounded-[20px] bg-[#f6f6f6] overflow-hidden drop-shadow-lg"
+              >
+                <div className="flex items-center h-[64px] bg-[#5458F7] text-white">
+                  <div className="flex items-center justify-center
+                    w-[38px] h-[38px] ml-[18px] bg-white rounded-full">
+                    <Image
+                      src="/purpleAvatar.png"
+                      width={29}
+                      height={24}
+                      alt="purple avatar"
+                    />
+                  </div>
+                  <div
+                    className="text-[20px] ml-[18px] font-bold">
+                    你的名字
+                  </div>
+                </div>
+                <Link
+                  href="/"
+                  className="flex items-center h-[64px] pl-[24px] text-[22px]"
+                >
+                  查看個人檔案
+                </Link>
+                <div className="w-[234px] self-center border-t border-[#D1CACE]" />
+                <button
+                  className="flex items-center h-[64px] pl-[24px] text-[22px]"
+                >
+                  登出
+                </button>
+              </nav>
+            </div>
+        }
+      </div>
     </div>
   );
 };
