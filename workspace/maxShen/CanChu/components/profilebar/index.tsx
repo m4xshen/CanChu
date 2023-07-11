705bb96179c3506d51ab3ef6d0ec7f2e3f30dc73
Max Shen <m4xshen@gmail.com>
2023-07-11 14:50:56 +0800

    Add hover text and effect on profile photo

--- workspace/maxShen/CanChu/components/profilebar/index.tsx
+++ workspace/maxShen/CanChu/components/profilebar/index.tsx
@@ -6,13 +6,22 @@ function Profilebar() {
     <div className="h-[23rem] px-32 bg-white mb-6 flex flex-col">
       <div className="flex items-center h-72 border-b border-[#C8C8C8] gap-11">
         <div className="rounded-full w-44 h-44 ml-7 overflow-hidden relative shrink-0">
-          <Image
-            src={profile.picture ? profile.picture : ''}
-            fill
-            sizes="5rem"
-            alt="user avatar"
-            className="object-cover"
-          />
+          <button type="button" className="group">
+            <Image
+              src={profile.picture ? profile.picture : ''}
+              fill
+              sizes="5rem"
+              alt="user avatar"
+              className="object-cover hover:brightness-50 group-hover:brightness-50"
+            />
+            <div
+              className="text-lg text-white underline whitespace-nowrap
+                absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+                hidden hover:block group-hover:block"
+            >
+              編輯大頭貼
+            </div>
+          </button>
         </div>
         <div className="flex flex-col justify-around">
           <div>
