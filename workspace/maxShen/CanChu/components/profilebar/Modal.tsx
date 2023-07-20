79d9743a14de7e0b219e3d3a5795730c2e7fb2c8
Max Shen <m4xshen@gmail.com>
2023-07-20 14:20:24 +0800

    refactor(profilebar): wrap modal into a component

---
+++ workspace/maxShen/CanChu/components/profilebar/Modal.tsx
@@ -0,0 +1,78 @@
+import AvatarEditor from 'react-avatar-editor';
+import { useRef } from 'react';
+
+import { NextRouter } from 'next/router';
+import CloseIcon from '../icons/CloseIcon';
+import useUpdatePicture from '@/hooks/useUpdatePicture';
+
+interface Props {
+  file: File;
+  setFile: React.Dispatch<React.SetStateAction<File | undefined>>;
+  router: NextRouter;
+}
+
+export default function Modal({ file, setFile, router }: Props) {
+  const updatePicture = useUpdatePicture();
+  const editor = useRef<AvatarEditor>(null);
+
+  return (
+    <>
+      <div className="absolute left-0 top-0 z-10 h-full w-full bg-black opacity-30" />
+      <div
+        className="absolute left-1/2 top-1/2 z-10 flex w-[30rem]
+        -translate-x-1/2 -translate-y-1/2 transform flex-col items-center
+        rounded-2xl border border-[#00000019] bg-white shadow-xl"
+      >
+        <button
+          type="button"
+          className="absolute right-6 top-6"
+          onClick={() => {
+            setFile(undefined);
+          }}
+        >
+          <CloseIcon />
+        </button>
+        <div className="my-6 flex items-center justify-center text-2xl font-extralight ">
+          編輯頭像
+        </div>
+        <AvatarEditor
+          ref={editor}
+          image={file}
+          width={330}
+          height={330}
+          border={[75, 0]}
+          borderRadius={9999}
+          color={[0, 0, 0, 0.5]}
+        />
+        <button
+          type="button"
+          className="my-4 h-10 w-36 rounded-md bg-[#5458F7] text-white"
+          onClick={() => {
+            if (!editor?.current) {
+              return;
+            }
+
+            const canvas = editor.current.getImage();
+            canvas.toBlob(async (blob: Blob | null) => {
+              if (!blob) {
+                return;
+              }
+
+              const res = await updatePicture(blob);
+
+              if (res.status === 413) {
+                alert('檔案大小太大');
+                setFile(undefined);
+                return;
+              }
+
+              router.reload();
+            });
+          }}
+        >
+          上傳
+        </button>
+      </div>
+    </>
+  );
+}
--- workspace/maxShen/CanChu/components/profilebar/index.tsx
+++ workspace/maxShen/CanChu/components/profilebar/index.tsx
@@ -1,26 +1,22 @@
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
-import AvatarEditor from 'react-avatar-editor';
 
+import Modal from './Modal';
 import useGetPicture from '@/hooks/useGetPicture';
-import useUpdatePicture from '@/hooks/useUpdatePicture';
 import { ProfileType } from '@/types';
-import CloseIcon from '../icons/CloseIcon';
 
 interface Props {
   profile: ProfileType | undefined;
 }
 
 function Profilebar({ profile }: Props) {
+  const router = useRouter();
   const picture = useGetPicture(profile?.id);
   const inputRef = useRef<HTMLInputElement>(null);
-  const router = useRouter();
   const [file, setFile] = useState<File>();
-  const editor = useRef<AvatarEditor>(null);
-  const updatePicture = useUpdatePicture();
 
-  // toggle moal
+  // toggle modal
   useEffect(() => {
     if (file) {
       document.body.classList.add('modal-open');
@@ -31,66 +27,7 @@ function Profilebar({ profile }: Props) {
 
   return (
     <>
-      {file && (
-        <>
-          <div className="absolute left-0 top-0 z-10 h-full w-full bg-black opacity-30" />
-          <div
-            className="absolute left-1/2 top-1/2 z-10 flex w-[30rem]
-              -translate-x-1/2 -translate-y-1/2 transform flex-col items-center
-              rounded-2xl border border-[#00000019] bg-white shadow-xl"
-          >
-            <button
-              type="button"
-              className="absolute right-6 top-6"
-              onClick={() => {
-                setFile(undefined);
-              }}
-            >
-              <CloseIcon />
-            </button>
-            <div className="my-6 flex items-center justify-center text-2xl font-extralight ">
-              編輯頭像
-            </div>
-            <AvatarEditor
-              ref={editor}
-              image={file}
-              width={330}
-              height={330}
-              border={[75, 0]}
-              borderRadius={9999}
-              color={[0, 0, 0, 0.5]}
-            />
-            <button
-              type="button"
-              className="my-4 h-10 w-36 rounded-md bg-[#5458F7] text-white"
-              onClick={() => {
-                if (!editor?.current) {
-                  return;
-                }
-
-                const canvas = editor.current.getImage();
-                canvas.toBlob(async (blob: Blob | null) => {
-                  if (!blob) {
-                    return;
-                  }
-
-                  const res = await updatePicture(blob);
-
-                  if (res.status === 413) {
-                    alert('檔案大小太大');
-                    setFile(undefined);
-                    return;
-                  }
-
-                  router.reload();
-                });
-              }}
-            >
-              上傳
-            </button>
-          </div>
-        </>
-      )}
+      {file && <Modal file={file} setFile={setFile} router={router} />}
       <div className="mb-6 flex h-[23rem] flex-col bg-white px-32">
         <div className="flex h-72 items-center gap-11 border-b border-[#C8C8C8]">
           <form
