3ef1dc44bd31013c7fca55f9a89081824bb7afcd
Max Shen <m4xshen@gmail.com>
2023-07-28 10:47:27 +0800

    feat(useposts): log when scroolling to bottom

--- workspace/maxShen/CanChu/hooks/usePosts.ts
+++ workspace/maxShen/CanChu/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
 import { PostType } from '@/types';
@@ -8,11 +8,33 @@ export default function usePosts(
   userId: number | null | undefined,
 ): PostType[] {
   const [nextCursor, setNextCursor] = useState();
+  const [isBottom, setIsBottom] = useState(false);
+
+  // handle scroll event
+  useEffect(() => {
+    function handleScroll() {
+      const within100pxFromBottom =
+        window.innerHeight + Math.round(window.scrollY) >=
+        document.body.offsetHeight - 100;
+      if (within100pxFromBottom) {
+        setIsBottom(true);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    if (isBottom) {
+      console.log('get the posts!');
+    }
+  }, [isBottom]);
+
   const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
   const url = userId
     ? `${apiDomain}/posts/search?user_id=${userId}`
     : `${apiDomain}/posts/search`;
-
   const { data, error, isLoading } = useSWR(url, fetcher);
 
   if (isLoading || error) {
