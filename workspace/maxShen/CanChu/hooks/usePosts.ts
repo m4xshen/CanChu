f055b8455781a50fb8ed03d3b65fbfda6c678678
Max Shen <m4xshen@gmail.com>
2023-07-28 10:36:39 +0800

    feat(useposts): save and print next_cursor

--- workspace/maxShen/CanChu/hooks/usePosts.ts
+++ workspace/maxShen/CanChu/hooks/usePosts.ts
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import useSWR from 'swr';
+
 import { PostType } from '@/types';
 import { fetcher } from '@/utils';
 
 export default function usePosts(
   userId: number | null | undefined,
 ): PostType[] {
+  const [nextCursor, setNextCursor] = useState();
   const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN;
   const url = userId
     ? `${apiDomain}/posts/search?user_id=${userId}`
@@ -14,6 +17,11 @@ export default function usePosts(
 
   if (isLoading || error) {
     return [];
+  }
+
+  if (data.data.next_cursor !== nextCursor) {
+    setNextCursor(data.data.next_cursor);
+    console.log(data.data.next_cursor);
   }
 
   return data?.data?.posts;
